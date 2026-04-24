6d25ca6b5f94f0f406ba910caaa641ce7927c7b2
Dan Farr <hi@drfarr.uk>
2026-04-24 18:24:38 +0000

    feat: export public api from @contextd/core

---
+++ packages/core/src/index.ts
@@ -0,0 +1,16 @@
+export {
+  CONTEXT_DIR,
+  findRoot,
+  loadContextFile,
+  loadAllContext,
+  getRelevantContext,
+  getContextStats,
+  buildExportOutput,
+} from './context.js';
+
+export type {
+  ContextMeta,
+  ContextFile,
+  AllContext,
+  ContextStats,
+} from './context.js';
